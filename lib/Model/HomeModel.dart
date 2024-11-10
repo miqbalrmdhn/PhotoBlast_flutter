615a81e11ef0c3f1799c949e3c089375eb06a374
tb-sirait <tbsintheworld@gmail.com>
2024-11-10 21:12:21 +0700

    apakah bisa berubah?

--- lib/Model/HomeModel.dart
+++ lib/Model/HomeModel.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class HomeModel {
   // Add any data or logic related to the home screen here
+  print("aw") {
+    // TODO: implement print
+    throw UnimplementedError();
+  }
 }
